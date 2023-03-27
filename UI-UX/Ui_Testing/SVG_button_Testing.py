11ea883b9d7fd9ba480ac7ebbfba34084083e0bb
RutvijP <pathakrutvij541@gmail.com>
2023-03-27 13:27:53 +0530

    Testing_Integration

---
+++ UI-UX/Ui_Testing/SVG_button_Testing.py
@@ -0,0 +1,26 @@
+from PyQt5.QtWidgets import QWidget, QApplication, QHBoxLayout
+from pyqt_svg_button.svgButton import SvgButton
+
+class SvgButtonExample(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.__initUi()
+    def __initUi(self):
+        newButton = SvgButton()
+        newButton.setIcon('new.svg')
+        openButton = SvgButton()
+        openButton.setIcon('open.svg')
+        saveButton = SvgButton()
+        saveButton.setIcon('save.svg')
+        lay = QHBoxLayout()
+        lay.addWidget(newButton)
+        lay.addWidget(openButton)
+        lay.addWidget(saveButton)
+        self.setLayout(lay)
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    ex = SvgButtonExample()
+    ex.show()
+    sys.exit(app.exec_())
---
+++ UI-UX/Ui_Testing/UI_Notes.md
@@ -0,0 +1,3 @@
+## Commands to be executed to get the results.
+1. pip install absresgetter -
+2. pip install pyqt-svg-button
