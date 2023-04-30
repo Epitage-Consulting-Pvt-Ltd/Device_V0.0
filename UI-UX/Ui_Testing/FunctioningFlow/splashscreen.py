06e650334a118dadaefd97ce22f90fa4741f539c
SharmadG <mayureshgondkar@gmail.com>
2023-04-30 20:04:13 +0530

    close splashscreen.py once the menu button is clicked

--- UI-UX/Ui_Testing/FunctioningFlow/splashscreen.py
+++ UI-UX/Ui_Testing/FunctioningFlow/splashscreen.py
@@ -65,6 +65,7 @@ class MainWindow(QMainWindow):
         layout_g.addWidget(menu_button, 3, 0, 1, 2, Qt.AlignCenter)
         menu_button.setStyleSheet(BUTTON_STYLE)
         menu_button.clicked.connect(self.show_menu_grid_window)
+        menu_button.clicked.connect(self.close)
 
         # update the time and date labels every second
         timer = QTimer(self)
