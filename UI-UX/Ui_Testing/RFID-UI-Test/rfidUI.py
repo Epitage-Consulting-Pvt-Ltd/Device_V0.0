2706b7c8cbd7772bb3f1abefa73fad496834e877
RutvijP <pathakrutvij541@gmail.com>
2023-05-05 15:01:24 +0530

    RFID_test_Hardware Testing-editv1

--- UI-UX/Ui_Testing/RFID-UI-Test/rfidUI.py
+++ UI-UX/Ui_Testing/RFID-UI-Test/rfidUI.py
@@ -3,38 +3,32 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
-from splashscreen import MainWindow
 
-import MFRC522
+import mrfc522
 
 from RFIDDummyWindow import RFIDPromptDummy
 
 # Enter the RFID number that you want to match
-#rfid_number = reader.read()
+rfid_number = "1234567890"
 
 reader = SimpleMFRC522()
 
-#app = QApplication(sys.argv)
-#widget = RFIDPromptDummy("sudarshanlogo.png","Rajesh Peche", "Epitage.png")
-#widget.show()
-
-def show_splashscreen_window(self):
-	self.splashscreen_window=MenuWindow()
-	self.splashscreen_window.show()
+app = QApplication(sys.argv)
+widget = RFIDPromptDummy("sudarshanlogo.png","Rajesh Peche", "Epitage.png")
+widget.show()
 
 try:
     while True:
         print("Hold a tag near the reader")
         #id, text = reader.read()
-        id,text = reader.read() # Just for bench testing
+        id,text = rfid_number # Just for bench testing
         print("ID: %s\nText: %s" % (id,text))
-#        if str(id) == id:
-        print("RFID number matched!")
-        self.show_splashscreen_window()
-	#widget.show()
-        sleep(6)
-        #widget.hide()
-        #sleep(5)
+        if str(id) == rfid_number:
+            print("RFID number matched!")
+            widget.show()
+            sleep(6)
+            widget.hide()
+        sleep(5)
 except KeyboardInterrupt:
     GPIO.cleanup()
     raise
