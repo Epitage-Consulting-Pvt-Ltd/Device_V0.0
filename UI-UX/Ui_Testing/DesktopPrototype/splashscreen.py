8e6ac9093b66b817b04fdd4797ba0d7b874cd929
RutvijP <pathakrutvij541@gmail.com>
2023-05-29 16:14:46 +0530

    splashscreen-finalization

--- UI-UX/Ui_Testing/DesktopPrototype/splashscreen.py
+++ UI-UX/Ui_Testing/DesktopPrototype/splashscreen.py
@@ -1,16 +1,20 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLabel, QPushButton
-from PyQt5.QtGui import QIcon, QPixmap, QColor, QPalette, QFont
-from PyQt5.QtCore import Qt, QTimer, QDate, QTime
-from MenuGrid import MenuWindow
-from topband import topband
-from theme import BACKGROUND_COLOR, FOREGROUND_COLOR, ACCENT_COLOR, BUTTON_STYLE, TABLE_STYLE , WINDOW_BACKGROUND_COLOR, WINDOW_FOREGROUND_COLOR
 import sys
+
+from PyQt5.QtCore import Qt, QTimer, QDate, QTime
+from PyQt5.QtGui import QPixmap, QPalette
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLabel, QPushButton
+
+from MenuGrid import MenuWindow
+from theme import BUTTON_STYLE, WINDOW_BACKGROUND_COLOR, \
+    WINDOW_FOREGROUND_COLOR
+from topband import topband
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        topband(self)
+        topband(self, "test")
 
         # set window title
         self.setWindowTitle("Splash Screen")
@@ -24,19 +28,12 @@ class MainWindow(QMainWindow):
         palette.setColor(QPalette.WindowText, WINDOW_FOREGROUND_COLOR)
         self.setPalette(palette)
 
-        # create central widget
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
 
-        # create a grid layout with 4 rows and 2 columns
-        layout_g = QGridLayout()
-        central_widget.setLayout(layout_g)
-
-        # add logos to the first row
+        # add logos to the first row (central logo )
         logo1 = QLabel()
-        pixmap1 = QPixmap("image/ethos.jpg").scaled(370, 370, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        pixmap1 = QPixmap("images/ethos.jpg").scaled(370, 370, Qt.KeepAspectRatio, Qt.SmoothTransformation)
         logo1.setPixmap(pixmap1)
-        layout_g.addWidget(logo1, 1, 0, 3, 0, Qt.AlignCenter)
+        logo1.move(240,400)
 
         logo2 = QLabel()
         pixmap2 = QPixmap("images/gsk.jpg").scaled(50, 50, Qt.KeepAspectRatio, Qt.SmoothTransformation)
@@ -48,33 +45,27 @@ class MainWindow(QMainWindow):
         logo3.setPixmap(pixmap3)
         logo3.move(370, 10)
 
-
-        '''
         # add system time to the second row
         time_label = QLabel()
         time_label.setObjectName("timeLabel")
         time_label.setAlignment(Qt.AlignCenter)
-        time_label.setStyleSheet("fint-size: 32px;")
-        layout_g.addWidget(time_label, 2, 1, 1, 2)
-        '''
+        time_label.setStyleSheet("font-size: 32px; color: black;")
+        time_label.move(240,600)
 
         # add today's date to the third row
         date_label = QLabel()
         date_label.setObjectName("dateLabel")
         date_label.setAlignment(Qt.AlignCenter)
         date_label.setStyleSheet("font-size: 32px;")
-        layout_g.addWidget(date_label, 3, 0, 2, 4)
-
-
+        date_label.move(240,700)
 
         # add menu button to the fourth row
         menu_button = QPushButton("Menu")
         menu_button.setFixedSize(120, 40)
-        layout_g.addWidget(menu_button, 4, 0, 1, 4, Qt.AlignCenter)
+        menu_button.move(240,750)
         menu_button.setStyleSheet(BUTTON_STYLE)
         menu_button.clicked.connect(self.show_menu_grid_window)
         menu_button.clicked.connect(self.close)
-
 
         # update the time and date labels every second
         timer = QTimer(self)
@@ -82,24 +73,19 @@ class MainWindow(QMainWindow):
         timer.start(1000)
 
     def update_labels(self):
-
-        '''
         # update the time label with the current system time
         time_label = self.centralWidget().findChild(QLabel)
         current_time = QTime.currentTime().toString("hh:mm:ss")
         time_label.setText(current_time)
-        '''
 
         # update the date label with today's date
         date_label = self.centralWidget().findChild(QLabel, "dateLabel")
-        #date_label = self.centralWidget().findChild(QLabel, "dateLabel")
         current_date = QDate.currentDate().toString(Qt.DefaultLocaleLongDate)
         date_label.setText(current_date)
 
     def show_menu_grid_window(self):
         self.menu_grid_window = MenuWindow()
         self.menu_grid_window.show()
-
 
 
 if __name__ == '__main__':
--- UI-UX/Ui_Testing/DesktopPrototype/topband.py
+++ UI-UX/Ui_Testing/DesktopPrototype/topband.py
@@ -2,15 +2,20 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtGui import QPixmap
 
-def topband(self):
+def topband(self , windowtitle):
     label1 = QLabel(self)
     pixmap1 = QPixmap('Epitage.png')
-    label1.setPixmap(pixmap1.scaled(100, 150))
-    label1.move(50, 50)
+    label1.setPixmap(pixmap1.scaled(100, 150,aspectRatioMode=True))
+    label1.move(50, 20)
 
     label2 = QLabel(self)
     pixmap2 = QPixmap('sudarshanlogo.png')
-    label2.setPixmap(pixmap2.scaled(100, 150))
-    label2.move(300, 50)
+    label2.setPixmap(pixmap2.scaled(100, 150,aspectRatioMode=True))
+    label2.move(300, 20)
+
+    label3 = QLabel(windowtitle, self)
+    label3.move(240, 20)
+
+
 
 
