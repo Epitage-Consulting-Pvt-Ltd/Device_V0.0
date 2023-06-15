aba6e01595e297a6a7b01eb5da562ec824c3d92d
RutvijP <pathakrutvij541@gmail.com>
2023-06-15 16:40:59 +0530

    prototype-testing

---
+++ Device_V0.1/proto3/splashscreenv4.py
@@ -0,0 +1,87 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtGui import QPixmap, QFont
+from PyQt5.QtCore import Qt, QTimer, QDateTime
+from datetime import datetime
+from utilities.components import create_img_button
+
+class DisplayWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Set window dimensions
+        self.width = 480
+        self.height = 800
+        self.setGeometry(0, 0, self.width, self.height)
+
+        # Set background image
+        self.background_image = QLabel(self)
+        self.background_image.setPixmap(QPixmap("images/background.png"))
+        self.background_image.setGeometry(0, 0, self.width, self.height)
+
+        # Create label for date and time
+        self.date_time_label = QLabel(self)
+        self.date_time_label.setGeometry(3, 3, 160, 20)
+
+        # Set font for date and time label
+        font_small = QFont("inika", 15, QFont.Bold)
+        self.date_time_label.setFont(font_small)
+
+        # Set font for date and time label
+        font_big = QFont("inika", 20, QFont.Normal)
+
+        self.logoImage = QLabel(self)
+        self.logoImage.setPixmap(QPixmap("images/img.png"))
+        self.logoImage.setGeometry(50,154,381,277)
+
+
+
+        # Create label for additional date
+        self.additional_date_label = QLabel(self)
+        self.additional_date_label.setGeometry(137, 509, 300,26)
+        self.additional_date_label.setFont(font_big)
+        self.additional_date_label.setAlignment(Qt.AlignVCenter)
+
+        # Create label for time
+        self.time_label = QLabel(self)
+        self.time_label.setGeometry(207, 547, 300, 30)
+        self.time_label.setFont(font_big)
+
+        self.thumb_btn = create_img_button(self, 'images/MenuIcon.png',75,100, (190, 623), self.close,"Menu","#D9D9D9")
+
+        # Update date and time every second
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_date_time)
+        self.timer.start(1000)
+
+        # Initial date and time display
+        self.update_date_time()
+
+    def update_date_time(self):
+        # Get current date and time
+        current_datetime = datetime.now()
+
+        # Format the date as "14th June 2023"
+        formatted_date = current_datetime.strftime("%d{} %B %Y").format(
+            "th" if 10 <= current_datetime.day <= 19 else
+            {1: "st", 2: "nd", 3: "rd"}.get(current_datetime.day % 10, "th")
+        )
+
+        # Get the current day
+        current_day = current_datetime.strftime("%A")
+
+        # Format the time as "hh:mm:ss"
+        formatted_time = current_datetime.strftime("%H:%M:%S")
+
+        # Update date and time labels
+        current_datetime_str = f"{formatted_date} - {formatted_time}"
+        self.date_time_label.setText(current_datetime_str)
+        self.additional_date_label.setText(f"{current_day} , {formatted_date}")
+        self.time_label.setText(formatted_time)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = DisplayWindow()
+    window.show()
+    sys.exit(app.exec_())
--- UI-UX/V3_Testing/keyboard/topband_V3.py
+++ UI-UX/V3_Testing/keyboard/topband_V3.py
@@ -2,8 +2,6 @@ from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 
-
-
 class TopBandLayout(QWidget):
     def __init__(self, windowtitle, parent=None):
         super(TopBandLayout, self).__init__(parent)
@@ -17,13 +15,11 @@ class TopBandLayout(QWidget):
         label1.setFixedSize(pixmap1.width(), pixmap1.height())  # Set fixed size for label1
         layout.addWidget(label1, 0, 0 ,alignment=Qt.AlignLeft)
 
-
         label3 = QLabel(self)
         label3.setText(windowtitle)
         label3.setStyleSheet("color: #ffffff;background-color: #e68326;")
         label3.setAlignment(Qt.AlignCenter)
-        layout.addWidget(label3, 0, 1, alignment= Qt.AlignCenter)
-
+        layout.addWidget(label3, 0, 1, alignment=Qt.AlignCenter)
 
         label2 = QLabel(self)
         pixmap2 = QPixmap('img/gsk.jpg').scaled(150, 50, Qt.AspectRatioMode.KeepAspectRatio, Qt.SmoothTransformation)
@@ -32,8 +28,18 @@ class TopBandLayout(QWidget):
         label2.setFixedSize(pixmap2.width(), pixmap2.height())  # Set fixed size for label2
         layout.addWidget(label2, 0, 2, alignment=Qt.AlignRight)
 
-        layout.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+        bottom_left_label = QLabel(self)
+        bottom_left_label.setText("+91XXXXXX")
+        layout.addWidget(bottom_left_label, 1, 0, alignment=Qt.AlignBottom | Qt.AlignLeft)
 
+        bottom_right_label = QLabel(self)
+        bottom_right_label.setText("Epitage Consulting Pvt Ltd")
+        layout.addWidget(bottom_right_label, 1, 2, alignment=Qt.AlignBottom | Qt.AlignRight)
+
+        # Add a row stretch to ensure the bottom two items are visible
+        layout.setRowStretch(2, 1)
+
+        layout.setAlignment(Qt.AlignTop | Qt.AlignLeft)
 
         layout.setColumnStretch(0, 1)
         layout.setColumnStretch(1, 1)
